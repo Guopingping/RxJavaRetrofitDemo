d0d400c056f45c73bce9f9f49e9304702f30d2b0
unknown <>
2017-02-08 18:45:58 +0800

    Initial commit

---
+++ app/src/main/java/com/gp/rxjavaretrofitdemo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.gp.rxjavaretrofitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.gp.rxjavaretrofitdemo.entity.MovieEntity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.click_me_BN)
+    Button clickMeBN;
+    @Bind(R.id.result_TV)
+    TextView resultTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.click_me_BN)
+    public void onClick(){
+        getMovie();
+    }
+
+    public void getMovie(){
+        String baseUrl = "https://api.douban.com/v2/movie/";
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+
+        MovieService movieService = retrofit.create(MovieService.class);
+
+        Call<MovieEntity> call = movieService.getTopMovie(0,10);
+
+        call.enqueue(new Callback<MovieEntity>() {
+            @Override
+            public void onResponse(Call<MovieEntity> call, Response<MovieEntity> response) {
+                resultTV.setText(response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<MovieEntity> call, Throwable t) {
+                resultTV.setText(t.getMessage());
+            }
+        });
+    }
+}
